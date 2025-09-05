fe874a9f72059bb5ae57c5d51e3ad17548b5563b
Griss <grisshink@mail.ru>
2025-09-05 16:51:13 +0300

    Add open command

--- putin.c
+++ putin.c
@@ -10,12 +10,16 @@
     } while (0)
 
 #define ARRLEN(arr) (sizeof(arr)/sizeof(arr[0]))
+#define PATH_LEN 512
 
 ma_engine audio;
 ma_sound sound;
 
-char* running_filepath;
+char running_filepath[PATH_LEN] = {0};
 bool is_running = true;
+bool loop = false;
+float pitch = 100.0f;
+float volume = 100.0f;
 
 char* cut_and_get_next_word(char* inp) {
     if (!*inp) return inp;
@@ -32,8 +36,8 @@ void print_time(float t) {
     printf("%02d:%02d", (int)fmodf(t / 60.0f, 60.0f), (int)fmodf(t, 60.0f));
 }
 
-void print_current_time(void) {
-    if (!running_filepath || !ma_sound_is_playing(&sound)) {
+void print_status(void) {
+    if (!ma_sound_is_playing(&sound)) {
         printf("stopped\n");
         return;
     }
@@ -49,7 +53,7 @@ void print_current_time(void) {
     ma_sound_get_length_in_seconds(&sound, &t);
     print_time(t);
 
-    printf("] - %s", running_filepath);
+    printf("] - %s", *running_filepath != '\0' ? running_filepath : "unnamed");
     if (ma_sound_is_looping(&sound)) printf(" loop");
     printf("\n");
 }
@@ -58,22 +62,43 @@ void process_commands(char* command) {
     char* args = cut_and_get_next_word(command);
 
     if (command[0] == 'c') {
-        print_current_time();
+        print_status();
         return;
     } else if (command[0] == 's') {
         if (!ma_sound_is_playing(&sound)) ma_sound_start(&sound);
 
         ma_sound_seek_to_second(&sound, atof(args));
-        print_current_time();
+        print_status();
         return;
     } else if (command[0] == 'l') {
-        bool loop = !ma_sound_is_looping(&sound);
+        loop = !ma_sound_is_looping(&sound);
         ma_sound_set_looping(&sound, loop);
         printf("loop %s\n", loop ? "on" : "off");
         return;
+    } else if (command[0] == 'o') {
+        ma_sound_uninit(&sound);
+        *running_filepath = '\0';
+
+        char* argpos = args;
+        while (*argpos != '\0' && *argpos != '\n') argpos++;
+        *argpos = '\0';
+
+        if (ma_sound_init_from_file(&audio, args, MA_SOUND_FLAG_STREAM, NULL, NULL, &sound)) {
+            printf("cant load file \"%s\"\n", args);
+            return;
+        }
+        ma_sound_set_looping(&sound, loop);
+        ma_sound_set_volume(&sound, volume / 100.0f);
+        ma_sound_set_pitch(&sound, pitch / 100.0f);
+        strncpy(running_filepath, args, PATH_LEN);
+
+        ma_sound_start(&sound);
+        print_status();
+
+        return;
     } else if (command[0] == 'p') {
         if (args[0] == '\0') {
-            printf("pitch %.3f%%\n", ma_sound_get_pitch(&sound) * 100.0f);
+            printf("pitch %.3f%%\n", pitch);
             return;
         }
 
@@ -82,12 +107,13 @@ void process_commands(char* command) {
             printf("invalid percent\n");
             return;
         }
+        pitch = p;
         ma_sound_set_pitch(&sound, p / 100.0f);
         printf("pitch %.3f%%\n", p);
         return;
     } else if (command[0] == 'v') {
         if (args[0] == '\0') {
-            printf("volume %.3f%%\n", ma_sound_get_volume(&sound) * 100.0f);
+            printf("volume %.3f%%\n", volume);
             return;
         }
 
@@ -96,6 +122,7 @@ void process_commands(char* command) {
             printf("invalid percent\n");
             return;
         }
+        volume = v;
         ma_sound_set_volume(&sound, v / 100.0f);
         printf("volume %.3f%%\n", v);
         return;
@@ -112,16 +139,17 @@ int main(int argc, char** argv) {
         printf("usage: %s <music_file_path>\n", argv[0]);
         return 1;
     }
-    running_filepath = argv[1];
 
     if (ma_engine_init(NULL, &audio)) ball("failed to initialize audio engine.");
 
-    if (ma_sound_init_from_file(&audio, running_filepath, MA_SOUND_FLAG_STREAM, NULL, NULL, &sound)) {
-        ball("cant load file");
+    if (ma_sound_init_from_file(&audio, argv[1], MA_SOUND_FLAG_STREAM, NULL, NULL, &sound)) {
+        printf("cant load file %s\n", argv[1]);
+    } else {
+        ma_sound_start(&sound);
+        strncpy(running_filepath, argv[1], PATH_LEN);
     }
     
-    ma_sound_start(&sound);
-    print_current_time();
+    print_status();
 
     char inp_buf[256];
     while (is_running) {
